4526046b1a023a93c90a23dbec5f94ede9acea6f
allthesignals <wilburnforce@gmail.com>
2018-04-02 14:21:03 -0400

    Add linters, cleanup boilerplate style

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,12 +9,24 @@ module.exports = {
   ],
   extends: [
     'eslint:recommended',
-    'plugin:ember/recommended'
+    'airbnb',
+    'plugin:ember-best-practices/recommended'
   ],
   env: {
     browser: true
   },
   rules: {
+    'import/no-extraneous-dependencies': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'lines-around-directive': 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'prefer-arrow-callback': 0,
+    'no-underscore-dangle': 0,
+    'camelcase': 0,
+    'max-len': 0,
+    'array-callback-return': 0
   },
   overrides: [
     // node files
