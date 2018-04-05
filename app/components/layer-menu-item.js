6de925e87441ad31d1afcc3ba8975cf4206700cd
allthesignals <wilburnforce@gmail.com>
2018-04-05 12:16:27 -0400

    Updated layer-menu-item to accept and require a title

--- app/components/layer-menu-item.js
+++ app/components/layer-menu-item.js
@@ -2,8 +2,14 @@ import Component from '@ember/component';
 import { argument } from '@ember-decorators/argument';
 import { action } from '@ember-decorators/object';
 import { type } from '@ember-decorators/argument/type';
+import { required } from '@ember-decorators/argument/validation';
 
 export default class LayerMenuItemComponent extends Component {
+  @required
+  @argument
+  @type('string')
+  title;
+
   @argument
   @type('boolean')
   active = true;
--- tests/integration/components/layer-menu-item-test.js
+++ tests/integration/components/layer-menu-item-test.js
@@ -7,7 +7,7 @@ module('Integration | Component | layer-menu-item', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it opens and closes on click', async function(assert) {
-    await render(hbs`{{layer-menu-item}}`);
+    await render(hbs`{{layer-menu-item title='Foo'}}`);
     await click('.layer-menu-item-title');
     const content = find('.layer-menu-item-content');
     assert.equal(!!content, false);
@@ -19,11 +19,17 @@ module('Integration | Component | layer-menu-item', function(hooks) {
 
   test('it yields content when open', async function(assert) {
     await render(hbs`
-      {{#layer-menu-item}}
+      {{#layer-menu-item title='Foo'}}
         template block text
       {{/layer-menu-item}}
     `);
+    const content = find('.layer-menu-item-content').textContent.trim();
+    assert.equal(content, 'template block text');
+  });
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('it shows a title', async function(assert) {
+    await render(hbs`{{layer-menu-item title='Foo'}}`);
+    const title = await find('.layer-menu-item-title').textContent.trim();
+    assert.equal(title, 'Foo');
   });
 });
