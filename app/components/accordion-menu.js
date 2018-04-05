10aecc146c3b1770da01fd6215c97debfed00b5f
allthesignals <wilburnforce@gmail.com>
2018-04-05 10:50:21 -0400

    Solid accordion component; fully tested, TDD 🤡

--- app/components/accordion-menu.js
+++ app/components/accordion-menu.js
@@ -1,13 +1,20 @@
 import Component from '@ember/component';
 import { argument } from '@ember-decorators/argument';
 import { action } from '@ember-decorators/object';
+import { type } from '@ember-decorators/argument/type';
+import { required } from '@ember-decorators/argument/validation';
 
 export default class AccordionMenuComponent extends Component {
   @argument
-  visible = true;
+  open = true;
+
+  @argument
+  @required
+  @type('string')
+  title;
 
   @action
   toggleAccordion() {
-    this.toggleProperty('visible');
+    this.toggleProperty('open');
   }
 }
--- tests/integration/components/accordion-menu-test.js
+++ tests/integration/components/accordion-menu-test.js
@@ -7,7 +7,7 @@ module('Integration | Component | accordion-menu', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it toggles closed', async function(assert) {
-    await render(hbs`{{accordion-menu}}`);
+    await render(hbs`{{accordion-menu title='Foo'}}`);
     await click('.switch');
     const accordionContent = await find('.accordion-content');
 
@@ -15,10 +15,24 @@ module('Integration | Component | accordion-menu', function(hooks) {
   });
 
   test('it toggles open', async function(assert) {
-    await render(hbs`{{accordion-menu visible=false}}`);
+    await render(hbs`{{accordion-menu title='Foo' open=false}}`);
     await click('.switch');
     const accordionContent = await find('.accordion-content');
 
     assert.equal(!!accordionContent, true);
+  });
+
+  test('it renders the title', async function(assert) {
+    await render(hbs`{{accordion-menu open=false title='Foo'}}`);
+    const title = await find('.accordion-title').textContent.trim();
+
+    assert.equal(title, 'Foo');
+  });
+
+  test('it yields the block', async function(assert) {
+    await render(hbs`{{#accordion-menu title='Foo'}}Bar{{/accordion-menu}}`);
+    const accordionContent = await find('.accordion-content').textContent.trim();
+
+    assert.equal(accordionContent, 'Bar');
   });
 });
