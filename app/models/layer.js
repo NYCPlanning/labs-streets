2b49ac0e5d4a5a20177c71ae659c22e52e3282bd
allthesignals <wilburnforce@gmail.com>
2018-04-09 22:40:30 -0400

    Define layout visibility on init

--- app/models/layer.js
+++ app/models/layer.js
@@ -1,11 +1,24 @@
 import Model from 'ember-data/model';
-// import { computed } from '@ember-decorators/object';
+import { merge } from '@ember/polyfills';
 import { attr, belongsTo } from 'ember-decorators/data';
 import { alias } from 'ember-decorators/object/computed';
 
 export default class LayerModel extends Model {
+  constructor() {
+    super();
+
+    // inherit visibility from layer group
+    // and apply as mapbox style spec.
+    // check the transform for style
+    // to add more
+    const visible = this.get('visible');
+    this.set('style.layout.visibility', (visible ? 'visible' : 'none'));
+    const currentStyle = merge({}, this.get('style'));
+    this.set('style', currentStyle);
+  }
+
   @belongsTo('layer-group') layerGroup
-  @attr() style
+  @attr('mapbox-gl-style') style
 
   @alias('layerGroup.visible') visible
 
