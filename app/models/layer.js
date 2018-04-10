5044e98be81361b7dbda6117c78af6f1e7bc66c5
allthesignals <wilburnforce@gmail.com>
2018-04-10 15:05:17 -0400

    Setup computeds and new interface for style

--- app/models/layer.js
+++ app/models/layer.js
@@ -1,13 +1,26 @@
 import Model from 'ember-data/model';
-// import { computed } from '@ember-decorators/object';
+import { computed } from '@ember-decorators/object';
 import { attr, belongsTo } from 'ember-decorators/data';
-import { alias } from 'ember-decorators/object/computed';
+import { alias, oneWay } from 'ember-decorators/object/computed';
 
 export default class LayerModel extends Model {
   @belongsTo('layer-group') layerGroup
   @attr() style
 
+  @oneWay('style') originalStyle
   @alias('layerGroup.visible') visible
+  @alias('layerGroup.highlightable') highlightable
 
-  @alias('layerGroup.highlightable') highlightable;
+  @computed('style.{paint,layout,filter}')
+  get mapboxGlStyle() {
+    return this.get('style');
+  }
+
+  @alias('style.filter') filter
+  @alias('style.layout') layout
+  @alias('style.paint') paint
+
+  resetStyle() {
+    this.set('style', this.get('originalStyle'));
+  }
 }
