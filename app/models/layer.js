cda51b919ed1457d6d73b8db9e66964cc7a3003c
allthesignals <wilburnforce@gmail.com>
2018-04-09 22:41:56 -0400

    Revert "Define layout visibility on init"
    This reverts commit 2b49ac0e5d4a5a20177c71ae659c22e52e3282bd.

--- app/models/layer.js
+++ app/models/layer.js
@@ -1,24 +1,11 @@
 import Model from 'ember-data/model';
-import { merge } from '@ember/polyfills';
+// import { computed } from '@ember-decorators/object';
 import { attr, belongsTo } from 'ember-decorators/data';
 import { alias } from 'ember-decorators/object/computed';
 
 export default class LayerModel extends Model {
-  constructor() {
-    super();
-
-    // inherit visibility from layer group
-    // and apply as mapbox style spec.
-    // check the transform for style
-    // to add more
-    const visible = this.get('visible');
-    this.set('style.layout.visibility', (visible ? 'visible' : 'none'));
-    const currentStyle = merge({}, this.get('style'));
-    this.set('style', currentStyle);
-  }
-
   @belongsTo('layer-group') layerGroup
-  @attr('mapbox-gl-style') style
+  @attr() style
 
   @alias('layerGroup.visible') visible
 
