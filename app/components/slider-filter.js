2986dd7ad1e807585b06e6767346ec3e3de27564
Chris Whong <chris@whong.org>
2018-04-13 00:47:51 -0400

    use moment to parse epoch correctly

--- app/components/slider-filter.js
+++ app/components/slider-filter.js
@@ -1,28 +1,29 @@
 import Component from '@ember/component';
 import { argument } from '@ember-decorators/argument';
-import { action } from '@ember-decorators/object';
+import { action, computed } from '@ember-decorators/object';
 import { required } from '@ember-decorators/argument/validation';
 import moment from 'moment';
 
 const fromEpoch = function(number, format) {
-  return moment(number).format(format);
+  return moment(number, 'X').format(format);
 };
 
 const defaultStart = [-2082931200, 1518825600];
 
 export default class SliderFilterComponent extends Component {
+
   @required
   @argument
   layer;
+
+  start = defaultStart
+  min = defaultStart[0]
+  max = defaultStart[1]
 
   format = {
     to: number => fromEpoch(number, 'YYYY'),
     from: number => fromEpoch(number, 'YYYY'),
   }
-
-  start = defaultStart
-  min = defaultStart[0]
-  max = defaultStart[1]
 
   @action
   sliderChanged([min, max]) {
