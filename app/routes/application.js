d59e52b44b97f73b906effaf8fc4ff52416481ed
Andy Cochran <acochran@planning.nyc.gov>
2018-04-17 13:08:27 -0400

    trigger window resize event (for map resize) on route transitions

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
 import { hash } from 'rsvp';
 import normalizeCartoVectors from 'carto-promises-utility/utils/normalize-carto-vectors';
+import { action } from '@ember-decorators/object';
+import { next } from '@ember/runloop';
 
 export default class ApplicationRoute extends Route {
   model = async function() {
@@ -18,6 +20,13 @@ export default class ApplicationRoute extends Route {
       layers,
       layerGroups,
       amendmentsFill,
+    });
+  }
+
+  @action
+  didTransition() {
+    next(function() {
+      window.dispatchEvent(new Event('resize'));
     });
   }
 }
