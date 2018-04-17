afcadde5ead5f12552ba2d0fa6a39e2aac8416b0
allthesignals <wilburnforce@gmail.com>
2018-04-16 21:17:53 -0400

    Add drag drop helper

--- tests/acceptance/user-toggles-layers-test.js
+++ tests/acceptance/user-toggles-layers-test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
 import { visit, click, triggerEvent, pauseTest } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import { drag } from '../../tests/helpers/drag-drop';
 
 module('Acceptance | user toggles layers', function(hooks) {
   setupApplicationTest(hooks);
@@ -11,7 +12,16 @@ module('Acceptance | user toggles layers', function(hooks) {
     await click('.layer-street-names .layer-menu-item-title');
     await click('.layer-name-changes .layer-menu-item-title');
     await click('.layer-city-map-alterations .layer-menu-item-title');
-    await triggerEvent('.noUi-handle-lower', 'dragstart', 1,1);
+    // await triggerEvent('.noUi-handle-lower', 'dragstart', event);
+
+    await drag('.noUi-handle-lower', {
+      drop: '.noUi-ltr',
+      dragOverMoves: [
+        [{ clientX: 1, clientY: 20 }],
+        [{ clientX: -50, clientY: 20 }]
+      ],
+    });
+
     await pauseTest();
     assert.equal('/', '/');
   });
