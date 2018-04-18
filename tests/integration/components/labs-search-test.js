a97fb4a87fc1075123b4777b67ed7904bd7773ea
allthesignals <wilburnforce@gmail.com>
2018-04-18 09:32:27 -0400

    Rename files, refactor

--- tests/integration/components/labs-search-test.js
+++ tests/integration/components/labs-search-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('Integration | Component | labs-geosearch', function(hooks) {
+module('Integration | Component | labs-search', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function(assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`{{labs-geosearch}}`);
+    await render(hbs`{{labs-search}}`);
 
     assert.equal(this.element.textContent.trim(), '');
 
     // Template block usage:
     await render(hbs`
-      {{#labs-geosearch}}
+      {{#labs-search}}
         template block text
-      {{/labs-geosearch}}
+      {{/labs-search}}
     `);
 
     assert.equal(this.element.textContent.trim(), 'template block text');
