dcd5754a3fa163eff021cf2d74447b9aa97fdaf9
Andy Cochran <acochran@planning.nyc.gov>
2018-04-27 12:07:35 -0400

    fix map-popup-content tests

--- tests/integration/components/map-popup-content-test.js
+++ tests/integration/components/map-popup-content-test.js
@@ -6,20 +6,13 @@ import hbs from 'htmlbars-inline-precompile';
 module('Integration | Component | map-popup-content', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders a spinner', async function(assert) {
-    this.set('popupFeatures', null); // popup features is set to null prior to API call
-    await render(hbs`{{map-popup-content features=popupFeatures }}`);
-    const spinner = await find('.fa-spin');
-    assert.equal(!!spinner, true);
-  });
-
   test('it renders a no-ammendments message', async function(assert) {
     this.set('popupFeatures', []);
     await render(hbs`{{map-popup-content features=popupFeatures }}`);
     assert.equal(this.element.textContent.trim(), 'There are no map amendments here.');
   });
 
-  test('it renders ammendments', async function(assert) {
+  test('it renders map alterations', async function(assert) {
     this.set('popupFeatures', [
       {
         'type': 'Feature',
@@ -28,6 +21,7 @@ module('Integration | Component | map-popup-content', function(hooks) {
           'coordinates': [[[-73, 40],[-73, 41],[-74, 41],[-74, 40]]]
         },
         'properties': {
+          'type': 'alteration',
           'name': 'Party',
           'altmappdf': 'like-its.pdf',
           'effective': '1999-01-01T05:00:00Z'
@@ -40,11 +34,11 @@ module('Integration | Component | map-popup-content', function(hooks) {
     const header = await find('.popup-header');
     assert.equal(header.textContent.trim(), 'Map Amendments');
 
-    const altmappdf = await find('.popup-list a');
+    const altmappdf = await find('.popup-list .filename');
     assert.equal(altmappdf.textContent.trim(), 'like-its.pdf');
 
     const effective = await find('.popup-list .date');
-    assert.equal(effective.textContent.trim(), 'Jan 1, 1999');
+    assert.equal(effective.textContent.trim(), 'Effective: Jan 1, 1999');
   });
 
 });
