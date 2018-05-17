2de42f8ecf7bae49335607710ece451a295af23b
Andy Cochran <acochran@planning.nyc.gov>
2018-05-17 11:49:03 -0400

    rename route features -> data

--- tests/unit/routes/data-test.js
+++ tests/unit/routes/data-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | features', function(hooks) {
+module('Unit | Route | data', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:features');
+    let route = this.owner.lookup('route:data');
     assert.ok(route);
   });
 });
