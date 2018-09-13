ae731a04fd1d30eb6a9a40c1e0b10c53eb3e75f5
Andy Cochran <acochran@planning.nyc.gov>
2018-09-13 11:38:32 -0400

    use labs-ui/site-header, destroy consuming app's labs-site-header

--- app/components/labs-site-header.js
+++
@@ -1,7 +0,0 @@
-import Component from '@ember/component';
-import { argument } from '@ember-decorators/argument';
-
-export default class LabsSiteHeaderComponent extends Component {
-  classNames = ['site-header'];
-  @argument closed = true;
-}
