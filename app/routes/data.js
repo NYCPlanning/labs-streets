89b33042756ca66715e137489bcc6c34a9cb2578
Chris Whong <chris@whong.org>
2018-09-14 16:28:25 -0400

    get source metadata on the data page

--- app/routes/data.js
+++ app/routes/data.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    const sources = this.store.peekAll('source');
+    return sources.toArray().uniqBy('meta.description');
+  },
 });
