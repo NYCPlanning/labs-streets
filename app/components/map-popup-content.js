388db435da8f48e65e0dfeeb538bdf74d8ee0abb
Bryan Marchena <bmarchena@planning.nyc.gov>
2022-08-29 11:40:46 -0400

    Display local law names and dates correctly

--- app/components/map-popup-content.js
+++ app/components/map-popup-content.js
@@ -88,7 +88,7 @@ export default class MapPopupContent extends Component {
       .filter(d => d.properties.type === 'streetnamechange');
 
     streetNameChanges.forEach((d) => {
-      d.properties.ll_effecti = moment(d.properties.ll_effecti, 'MM/DD/YYYY').format('MMM D, YYYY'); // eslint-disable-line
+      d.properties.lleffectdt = moment(d.properties.lleffectdt, 'MM/DD/YYYY').format('MMM D, YYYY'); // eslint-disable-line
       return d;
     });
 
