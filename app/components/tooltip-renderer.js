0435c60773fec157ae2a16b1390e902e516c6a68
tangoyankee <miller.tim108@gmail.com>
2023-04-21 15:09:31 -0400

    mustache direct dependency

--- app/components/tooltip-renderer.js
+++ app/components/tooltip-renderer.js
@@ -1,15 +1,12 @@
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { argument } from '@ember-decorators/argument';
-import mustache from 'npm:mustache';
+import mustache from 'mustache';
 
 export default class TooltipRenderer extends Component {
   @computed('feature', 'template')
   get renderedText() {
-    const properties = this.get('feature.properties');
-    const template = this.get('template');
-
-    return mustache.render(template, properties);
+    return mustache.render(this.template, this.properties);
   }
 
   @argument
