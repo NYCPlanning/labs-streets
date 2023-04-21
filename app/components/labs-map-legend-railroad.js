26d19262659fe6d98a2f99f9b4ab215f68335eeb
tangoyankee <miller.tim108@gmail.com>
2023-04-21 15:06:48 -0400

    remove ember-decorators and unused query macro

--- app/components/labs-map-legend-railroad.js
+++ app/components/labs-map-legend-railroad.js
@@ -1,18 +1,19 @@
 import Component from '@ember/component';
 import { argument } from '@ember-decorators/argument';
-import { tagName, classNames, attribute } from '@ember-decorators/component';
 import layout from '../templates/components/labs-map-legend-railroad';
 
 export default
-@tagName('svg')
-@classNames('legend-icon', 'line-array')
 class LabsMapLegendLineComponent extends Component {
-  layout = layout
+  tagName = 'use';
+  classNames = ['legend-icon', 'line-array'];
+  layout = layout;
 
-  @attribute height = 10;
-  @attribute width = 17;
-  @attribute viewBox = '0 0 17 10';
-  @attribute preserveAspectRatio = 'xMinYMid';
+  attributeBindings = ['height', 'width', 'viewBox', 'preserveAspectRatio']
+
+  height = 10;
+  width = 17;
+  viewBox = '0 0 17 10';
+  preserveAspectRatio = 'xMinYMid';
 
   @argument style = {};
 
