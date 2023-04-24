8e86f43ae0fbadc48cf03581cbe56da36a89059a
tangoyankee <miller.tim108@gmail.com>
2023-04-24 12:22:12 -0400

    v3.1.0-beta.1...v3.1.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -40,8 +40,8 @@ module.exports = {
     // node files
     {
       files: [
-        'testem.js',
         'ember-cli-build.js',
+        'testem.js',
         'config/**/*.js',
         'lib/*/index.js'
       ],
