5e1ea02004e4501ffe1af1c088e356181979f41f
tangoyankee <miller.tim108@gmail.com>
2023-04-24 13:06:45 -0400

    v3.3.0...v3.5.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,19 +5,19 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module',
     ecmaFeatures: {
-      experimentalObjectRestSpread: true
-    }
+      experimentalObjectRestSpread: true,
+    },
   },
   plugins: [
-    'ember'
+    'ember',
   ],
   extends: [
     'eslint:recommended',
     'airbnb-base',
-    'plugin:ember-best-practices/recommended'
+    'plugin:ember-best-practices/recommended',
   ],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {
     'import/no-extraneous-dependencies': 0,
@@ -28,32 +28,33 @@ module.exports = {
     'space-before-function-paren': 0,
     'prefer-arrow-callback': 0,
     'no-underscore-dangle': 0,
-    'camelcase': 0,
+    camelcase: 0,
     'max-len': 0,
     'array-callback-return': 0,
     'react/prefer-stateless-function': 0,
     'class-methods-use-this': 0,
     'ember-best-practices/require-dependent-keys': 0,
-    'react/sort-comp': 0
+    'react/sort-comp': 0,
   },
   overrides: [
     // node files
     {
       files: [
+        '.template-lintrc.js',
         'ember-cli-build.js',
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'lib/*/index.js'
+        'lib/*/index.js',
       ],
       parserOptions: {
         sourceType: 'script',
-        ecmaVersion: 2015
+        ecmaVersion: 2015,
       },
       env: {
         browser: false,
-        node: true
-      }
-    }
-  ]
+        node: true,
+      },
+    },
+  ],
 };
