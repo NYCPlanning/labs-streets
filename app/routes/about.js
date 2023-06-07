addff8e21333e98958af23c13aa6c82cfe23b80b
tangoyankee <miller.tim108@gmail.com>
2023-06-07 15:38:09 -0400

    page updates

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
-export default Route.extend({
-});
+export default class AboutRoute extends Route {
+  @action
+  didTransition() {
+    const applicationController = this.controllerFor('application');
+    applicationController.set('sidebarIsClosed', true);
+  }
+}
