9ef1ee510f63d98a3aef369857d8d19c6356aee2
tangoyankee <miller.tim108@gmail.com>
2023-06-07 15:36:36 -0400

    applicable tests

--- tests/unit/routes/index-test.js
+++ tests/unit/routes/index-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | about', function(hooks) {
+module('Unit | Route | index', function(hooks) {
   setupTest(hooks);
 
   test('it exists', function(assert) {
-    const route = this.owner.lookup('route:about');
+    const route = this.owner.lookup('route:index');
     assert.ok(route);
   });
 });
--- tests/unit/serializers/application-test.js
+++ tests/unit/serializers/application-test.js
@@ -1,23 +1,22 @@
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
-import { run } from '@ember/runloop';
 
 module('Unit | Serializer | application', function(hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  skip('it exists', function(assert) {
-    let store = this.owner.lookup('service:store');
-    let serializer = store.serializerFor('application');
+  test('it exists', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const serializer = store.serializerFor('application');
 
     assert.ok(serializer);
   });
 
-  skip('it serializes records', function(assert) {
-    let store = this.owner.lookup('service:store');
-    let record = run(() => store.createRecord('application', {}));
+  test('it serializes records', function(assert) {
+    const store = this.owner.lookup('service:store');
+    const record = store.createRecord('application', {});
 
-    let serializedRecord = record.serialize();
+    const serializedRecord = record.serialize();
 
     assert.ok(serializedRecord);
   });
