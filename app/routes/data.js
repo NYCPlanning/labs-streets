9b155f667994c7eff4eda44b1c299e11ba9427ba
tangoyankee <miller.tim108@gmail.com>
2023-06-07 15:38:09 -0400

    page updates

--- app/routes/data.js
+++ app/routes/data.js
@@ -1,8 +1,15 @@
 import Route from '@ember/routing/route';
+import { action } from '@ember/object';
 
-export default Route.extend({
-  model() {
+export default class DataRoute extends Route {
+  async model() {
     const sources = this.store.peekAll('source');
     return sources.toArray().uniqBy('meta.description');
-  },
-});
+  }
+
+  @action
+  didTransition() {
+    const applicationController = this.controllerFor('application');
+    applicationController.set('sidebarIsClosed', true);
+  }
+}
