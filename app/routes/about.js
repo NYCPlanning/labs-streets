6ff427fc92fea7455bc28c73e29b94f5e1b24f68
tangoyankee <miller.tim108@gmail.com>
2023-06-08 12:51:30 -0400

    remove sidebar state from data and about pages

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,10 +1,4 @@
 import Route from '@ember/routing/route';
-import { action } from '@ember/object';
 
 export default class AboutRoute extends Route {
-  @action
-  didTransition() {
-    const applicationController = this.controllerFor('application');
-    applicationController.set('sidebarIsClosed', true);
-  }
 }
